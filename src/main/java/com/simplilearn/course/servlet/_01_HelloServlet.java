444ef605e59a5abf65cf36fe2cf21776a1dc9fe9
cclarke74 <clairc2@gmail.com>
2023-10-15 16:45:40 -0700

    Add Parameter and HeaderInfo Servlets

--- src/main/java/com/simplilearn/course/servlet/_01_HelloServlet.java
+++ src/main/java/com/simplilearn/course/servlet/_01_HelloServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-public class HelloServlet extends HttpServlet {
+public class _01_HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
