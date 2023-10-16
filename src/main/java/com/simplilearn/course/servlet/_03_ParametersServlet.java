9cb033846a9a6c5a770e25d435c0aa46880190c5
cclarke74 <clairc2@gmail.com>
2023-10-15 17:29:40 -0700

    Added FormServlet

--- src/main/java/com/simplilearn/course/servlet/_03_ParametersServlet.java
+++ src/main/java/com/simplilearn/course/servlet/_03_ParametersServlet.java
@@ -9,29 +9,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation class ParametersServlet
+ */
+
 @WebServlet("/ParametersServlet")
 public class _03_ParametersServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 
-			String firstName = request.getParameter("firstName");
-			String lastName = request.getParameter("lastName");
-;		
-			PrintWriter out = response.getWriter();
-	       
-			out.print("<html>");
-			out.print("<body>");
-			out.print("<h1>Parameters HttpServlet Class Example</h1>");
-			out.print("<br>");
-			out.print("<h2>First Name: " + firstName + "</h2>");
-			out.print("<h2>Last Name: " + lastName + "</h2>");
-			out.print("</body>");
-			out.print("</html>");
-	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String firstName = request.getParameter("firstname");
+        String lastName = request.getParameter("lastname");
+
+        PrintWriter out = response.getWriter();
+
+        out.print("<html>");
+        out.print("<body>");
+        out.print("<h1>Parameters HttpServlet Class Example</h1>");
+        out.print("<br>");
+        out.print("<h2>First Name: " + firstName + "</h2>");
+        out.print("<h2>Last Name: " + lastName + "</h2>");
+        out.print("</body>");
+        out.print("</html>");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
 
 }
