6367e1d673b3cf84448484904b1941d22fc00ee4
cclarke74 <clairc2@gmail.com>
2023-10-23 17:11:29 -0700

    Added dbdataactions

--- src/main/java/com/simplilearn/course/servlet/_09_DBServlet.java
+++ src/main/java/com/simplilearn/course/servlet/_09_DBServlet.java
@@ -1,7 +1,6 @@
 package com.simplilearn.course.servlet;
 
 import java.io.IOException;
-
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -19,103 +18,104 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.simplilearn.course.bean.User;
 
-/*
- * Servlet implementation class DBServlet
+/**
+ * Servlet implementation class TokenServlet
  */
-
 @WebServlet("/DBServlet")
 public class _09_DBServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		boolean show =Boolean.valueOf(request.getParameter("show"));
 
-		if (show) {
-			List<User> users = this.loadUsers();  
-			request.setAttribute("users", users);
-		
-		}	
-		
-		request.getRequestDispatcher("user_db_data.jsp").forward(request, response);
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-		
-	
-	}
-	
-	
-	List<User> loadUsers(){
-	
-	String dbUrl = "jdbc:mysql://localhost:3306/mydb";
-	String dbUsername = "root";
-	String dbPassword = "";
-	
-	List<User> users = new ArrayList<>();
-	
-	/*
-	 * Create the SELECT SQL Statement
-	 */
-	String sql = "SELECT * FROM USER";
-	
-	                   
-	System.out.println("Query: " + sql);
-	try {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-	} catch (ClassNotFoundException e) {
-		e.printStackTrace();
-	}
-	
-	/*
-	 * Get data from the DB table;
-	 */
-	try(Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-		Statement st = con.createStatement();
-			ResultSet rs = st.executeQuery(sql)) {
-	
-		/*
-		 * Note that the Connection Statement and the ResultSet are closed
-		 * automatically by the try() with resources.
-		 */
-		
-		
-		while (rs.next()) {
-			int idUser = rs.getInt("ID_USER");
-			String username = rs.getString("USERNAME");
-			String password = rs.getString("PASSWORD");
-			String firstName = rs.getString("FIRST_NAME");
-			String lastName = rs.getString("LAST_NAME");
-			Date birth = rs.getDate("BIRTH");
-			String status = rs.getString("STATUS");
-			
-			/*
-			 * Create and populate a User object.
-			 */
-			User user = new User();
-			user.setIdUser(idUser);
-			user.setUsername(username);
-			user.setPassword(password);
-			user.setFirstName(firstName);
-			user.setLastName(lastName);
-			user.setBirth(birth);
-			user.setStatus(status);
-			
-			/*
-			 * Add the user to the users list
-			 */
-			
-			users.add(user);
-		}
-		
-		System.out.println("Number of rows retrieved: " + users.size());
-	} catch (SQLException ex) {
-		
-		System.err.println("Error while accessing the DB! , " + ex.getSQLState() + ", " + ex.getMessage());
-	} 	
+    	List<User> users = new ArrayList<>();
+    	
+        boolean show = Boolean.valueOf(request.getParameter("show"));
 
-	users.forEach(System.out::println);
-		return users;
-	}
+        if (show) {
+           users = this.loadUsers();
+        }
+
+        request.setAttribute("show", show);
+        request.setAttribute("users", users);
+
+        request.getRequestDispatcher("user_db_data_actions.jsp").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+
+    List<User> loadUsers(){
+
+        String dbUrl = "jdbc:mysql://localhost:3306/mydb";
+        String dbUsername = "root";
+        String dbPassword = "";
+
+        List<User> users = new ArrayList<>();
+
+        /*
+         * Create the Select SQL Statement
+         */
+        String sql = "SELECT * FROM USER";
+
+        System.out.println("Query: " + sql);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        /*
+         * Get data from the DB table;
+         */
+        try(Connection con = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql)) {
+
+            /*
+             * Note that the Connection, Statement and the ResultSet are closed
+             * automatically by the try() with resources.
+             */
+
+
+            while (rs.next()) {
+
+                int idUser = rs.getInt("ID_USER");
+                String username = rs.getString("USERNAME");
+                String password = rs.getString("PASSWORD");
+                String firstName = rs.getString("FIRST_NAME");
+                String lastName = rs.getString("LAST_NAME");
+                Date birth = rs.getDate("BIRTH");
+                String status = rs.getString("STATUS");
+
+                /*
+                 * Create and populate a User object.
+                 */
+                User user = new User();
+                user.setIdUser(idUser);
+                user.setUsername(username);
+                user.setPassword(password);
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+                user.setBirth(birth);
+                user.setStatus(status);
+
+                /*
+                 * Add the user to the users list
+                 */
+                users.add(user);
+            }
+
+            System.out.println("Number of rows retrieved: " + users.size());
+        } catch (SQLException ex) {
+
+            System.err.println("Error while accessing the DB! , " + ex.getSQLState() + ", " + ex.getMessage());
+        }
+
+        users.forEach(System.out::println);
+
+        return users;
+    }
 }
